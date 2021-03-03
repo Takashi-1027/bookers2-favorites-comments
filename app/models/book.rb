fbc56b1fadab734fd6e893711961a06a7e112852
Takashi-1027 <taka19921027@gmail.com>
2021-03-03 09:30:27 +0000

    [いいね機能を追加]途中

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,17 @@ class Book < ApplicationRecord
   belongs_to :user
   #  attachment :image #いるかわからない！画像アップ用のメソッド（attachment）を追加してフィールド名に（image）を指定する。refileを使用するうえでのルール。
 
+  has_many :favorites, dependent: :destroy
+
     validates :title, presence: true
     validates :title, length: {maximum: 200}
 
     validates :body, presence: true
     validates :body, length: {maximum: 200}
+
+  def favorited_by?(user) # favorited_by?メソッドで引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べます。存在していればtrue、存在していなければfalseを返すようにしています。
+    favorites.where(user_id: user.id).exists?
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
-  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get 'users/show'
 
-  resources :books, only: [:new, :create, :index, :show, :destroy ,:edit, :update]
-  resources :users, only: [:show, :edit, :update, :index]
 
+  resources :users    #, only: [:show, :edit, :update, :index]
+
+  resources :books do  #, only: [:new, :create, :index, :show, :destroy ,:edit, :update]
+    resource :favorites, only: [:create, :destroy]
+    # resources :comments, only: [:create, :destroy]
+  end
 end
   #get 'users/edit'
   #get 'users/index'
@@ -18,4 +20,7 @@ end
   #get 'books/edit'
   #get 'homes/about'
   #get 'homes/top'
+
+  # get 'favorites/create'
+  # get 'favorites/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
